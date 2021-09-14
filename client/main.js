5c46b88a1d59b75ca9bc58d165dfb2c6abe96d2f
conto <constantijn@vanhartesveldt.nl>
2021-09-14 10:14:52 +0200

    added styles

---
+++ client/main.js
@@ -0,0 +1,28 @@
+/*
+this script listens to the following actions
+b
+a
+start 
+select
+dpad up
+dpad down 
+dpad left
+dpad right
+
+*/
+
+// the right 2 buttons
+const ButtonA = document.getElementById('ButtonA')
+const ButtonB = document.getElementById('ButtonB')
+
+// the middle buttons
+const ButtonStart = document.getElementById('ButtonStart')
+const ButtonSelect = document.getElementById('ButtonSelect')
+
+// dpad 
+const ButtonUp = document.getElementById('ButtonUp')
+const ButtonDown = document.getElementById('ButtonDown')
+const ButtonLeft = document.getElementById('ButtonLeft')
+const ButtonRight = document.getElementById('ButtonRight')
+
+const ButtonAll = document.getElementsByClassName('Button')
