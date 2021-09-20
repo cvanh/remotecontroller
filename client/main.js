c27a28717c4b3b912aa276e901ceb6686c437cc1
conto <constantijn@vanhartesveldt.nl>
2021-09-20 17:02:17 +0200

    working

--- client/main.js
+++ client/main.js
@@ -12,7 +12,7 @@ dpad right
 */
 
 // change the ip to ther current network ip
-ws = new WebSocket('ws://192.168.178.98:7072');
+ws = new WebSocket('ws://10.52.7.164:7072');
 
 // declares all the button seems to break when not present
 // the right 2 buttons
